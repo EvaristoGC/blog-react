0f84dbf3ab855aad3ebadc0701fd26973192ad5b
Evaristo GC <evaristogaell@gmail.com>
2024-10-01 16:50:39 +0200

    initial struture: created Navbar

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from './components/Navbar'
+
+import { Outlet } from 'react-router-dom'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='container'>
+        <Navbar />
+        <Outlet />
+      </div>
+    </div>
+  )
+}
+
+export default App
