3b30c7e8c59e2c7413dcd752251adac5dd6f38cd
Evaristo GC <evaristogaell@gmail.com>
2024-10-01 18:46:56 +0200

    Receiving data from the API: title and body created

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import axios from "axios"
+
+import './Home.css'
 
 const Home = () => {
+
+  const [posts, setPosts] = useState([])
+
+  const getPosts = async () => {
+    try {
+
+      const url = "https://jsonplaceholder.typicode.com/posts"
+      const response = await axios.get(url)
+      const data = response.data
+
+      setPosts(data)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+
+    getPosts()
+
+  }, [])
+
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? <p>Carregando...</p> : (
+        posts.map((posts) => (
+          <div className="post" key={posts.id}>
+            <h2>{posts.title}</h2>
+            <p>{posts.body}</p>
+            <Link to={`/posts/${posts.id}`} className="btn">
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
   )
 }
 
